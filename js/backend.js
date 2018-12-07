0c73e8279d61277a7f2647abffc4fab69289b067
cheahpehao <killerwiz@hotmail.com>
2018-12-07 15:53:40 +0800

    added read from json and write to json

--- js/backend.js
+++ js/backend.js
@@ -1,7 +1,27 @@
 var http = require("http");
 var fs = require("fs");
-var obj = JSON.parse(fs.readFileSync('dummy.json','utf-8'));
+var parseObj = fs.readFile('dummy.json','utf-8',function(err,data){
+    console.log(typeof(data));
+    var newJSON = [];
+    var newObj = {
+        "name":"Restaurant CDE",
+        "business-hour":"9am-11pm",
+        "address":"10, Jalan Chan Sow Lin",
+        "location":"Kepong"
+    }
+    newJSON.push(JSON.parse(data)); //JSON.parse from string type back to object type
+    newJSON.push(newObj); //Push new object into JSON
+    for (i=0;i<newJSON.length;i++){
+        console.log("Showing new JSON: " + newJSON[i].name + newJSON[i].address);
+    };
+    var writeJSON = JSON.stringify(newJSON); //Stringify it before writing
+    fs.writeFile('newJson.json',writeJSON,function(err,data){ //Export to new Json file
+        if (err) throw err;
+        console.log("New json written");
+    })
+});
 
+var obj = JSON.parse(fs.readFileSync('dummy.json','utf-8'));
 var server = http.createServer(function(req,res){
     if(req.url==='/home' || red.url === '/'){
         res.writeHead(200,{'content-type':'text/html'});
